d7da050543edd7309e47cc8d92e6f0b2a21229b7
xiaohuzheng <zhengboy320@163.com>
2020-11-17 14:56:28 +0000

    提交作业

--- simplelayout/cli/cli_generate.py
+++ simplelayout/cli/cli_generate.py
@@ -3,7 +3,22 @@ import argparse
 
 def get_options():
     parser = argparse.ArgumentParser()
-    # TODO: 按 1-simplelayout-CLI 要求添加相应参数
-
+    parser.add_argument("--board_grid", type=int)
+    parser.add_argument("--unit_grid", type=int)
+    parser.add_argument("--unit_n", type=int)
+    parser.add_argument("--positions", type=int, nargs="+")
+    parser.add_argument("--outdir", type=str)
+    parser.add_argument("--file_name", type=str)
     options = parser.parse_args()
+
+    if args.board_grid % args.unit_grid != 0:
+        sys.exit()
+
+    upper_value = (options.board_grid / options.unit_grid)**2
+    if len(options.positions) == options.unit_n:
+        if min(options.positions) < 1 or max(options.positions) > upper_value:
+            sys.exit()
+    else:
+        sys.exit()
+
     return options
--- simplelayout/generator/utils.py
+++ simplelayout/generator/utils.py
@@ -9,14 +9,16 @@ import scipy.io as sio
 
 def save_matrix(matrix, file_name):
     # TODO: 存储 matrix 到 file_name.mat, mdict 的 key 为 "matrix"
-    raise NotImplementedError
+    sio.savemat('file_name.mat', {'matrix':matrix})
 
 
 def save_fig(matrix, file_name):
     # TODO: 将 matrix 画图保存到 file_name.jpg
-    raise NotImplementedError
+    plt.imshow(matrix)
+    plt.savefig(file_name)
 
 
 def make_dir(outdir):
     # TODO: 当目录 outdir 不存在时创建目录
-    raise NotImplementedError
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
